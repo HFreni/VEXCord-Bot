19b7edcb0b09d7c1de94716d71e0bf7db78a3a07
Harrison Freni <hfreni@chadwickschool.org>
2016-06-30 11:02:25 -0700

    Documented for the bae

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
+// Your run of the mill imports!
 var Discord = require("discord.io");
+
+// This is our auth token, and it's going on github :D
 var vexbot	= new Discord.Client({
 	token: "MTk4MTE5MTA0NDkyNzMyNDE2.ClbgSw.UhlPpN-LlwwiKvyRfllQ7i9dtd0",
 	autorun: true
 });
 
+// Make all of our commands as variables to make code as modular as possible.
+var commandChar 	= "|";
+var vexDBCommand	= commandChar + " VEXDB TEAM";
+
+
+// Initialization command.
+// This lets us know that the bot has connected to the discord server,
+// and has will let us have control over the presence of the bot.
 vexbot.on('ready', function() {
 	console.log(vexbot.username + " -  ( " + vexbot.id + ")" );
 	vexbot.setPresence({
 		idle_since: null,
-		game: "Harri is a gud programmer"
+		game: "10 BPS"
 	});
 });
 
+// This is the main listening function in which commands are read & seen by the bot
 vexbot.on('message', function(user, userID, channelID, message, event) {
-	if(message === "| VEXDB") {
-		vexbot.sendMessage({
+	// if message x is receieved print y
+	// Make this more dynamic.
+
+	// Unfinished command to search for user input and then call vexDB
+	if(message.search(vexDBCommand)) {
+		console.log("It worked");
+		bot.sendMessage({
 			to: channelID,
-			message: "pong"
+			message: "VEXDB Info Here"
 		});
-		vexbot.uploadFile({
-			to: channelID,
-			file: "dank.jpg",
-			filename: "mankdemes.jpg",
-			message: "What a good meme"
-		});
-		console.log("Message Recieved")
 	}
 });
