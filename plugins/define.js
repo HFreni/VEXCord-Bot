c8533cbb4350f6cccb4ea9722b210bd313a4bde4
Harrison Freni <harrison@harrisonfreni.me>
2016-07-01 00:31:55 -0700

    Added Urban Dict Definitions

---
+++ plugins/define.js
@@ -0,0 +1,36 @@
+var http = require('https');
+
+// Stealing Andre's Code Cuz Why Not?!?
+var definitionGet = function(url, cb) {
+	var options = {
+		host: 'mashape-community-urban-dictionary.p.mashape.com',
+		path: '/' + url,
+		headers: {"X-Mashape-Key": "WNsTcdAjW7mshAv0v8SOWo3FvoMgp1uUmXVjsn9FMLyYz4TCYN", "Accept" : "text/plain"}
+	};//?term=
+
+	console.log("requesting \"" + options.path + "\"");
+
+	callback = function(response) {
+		var str = '';
+		response.on('data', function(chunk) {
+			str += chunk;
+		});
+
+		response.on('end', function () {
+			console.log(str);
+			cb(true, JSON.parse(str));
+		});
+	}
+
+	var req = http.request(options, callback);
+	req.end();
+};
+
+vexBot.commands.define = function(data) {
+	definitionGet("define?term=" + encodeURIComponent(data.message), function(err, definition) {
+		var d = definition.list[0];
+		data.respond(
+			"Definition of " + data.message + " is: " + d.definition
+		);
+	})
+}
