c9943e998a89cde33bc1482e3755f1c23f508d48
Harrison Freni <harrison@harrisonfreni.me>
2016-07-01 01:12:03 -0700

    Added Logoff Script

--- plugins/define.js
+++ plugins/define.js
@@ -20,7 +20,7 @@ var definitionGet = function(url, cb) {
 			console.log(str);
 			cb(true, JSON.parse(str));
 		});
-	}
+	};
 
 	var req = http.request(options, callback);
 	req.end();
@@ -29,8 +29,21 @@ var definitionGet = function(url, cb) {
 vexBot.commands.define = function(data) {
 	definitionGet("define?term=" + encodeURIComponent(data.message), function(err, definition) {
 		var d = definition.list[0];
-		data.respond(
-			"Definition of " + data.message + " is: " + d.definition
-		);
-	})
-}
+    if(definition.result_type == "no_results"){
+      data.respond("There is no definition for the term: " + data.message);
+    } else {
+      //I'm so sorry, please don't hurt me.
+      if (data.message.substring(0, 1) == 'a' || data.message.substring(0, 1) == 'e' || data.message.substring(0, 1) == 'i' || data.message.substring(0, 1) == 'o' || data.message.substring(0, 1) == 'u') {
+        data.respond(
+          "The Definition of an " + data.message + " is: " + d.definition + "\n" +
+          "An Example of an " + data.message + " is: " + d.example
+        );
+      } else {
+        data.respond(
+          "The Definition of a " + data.message + " is: " + d.definition + "\n" +
+          "An Example of a " + data.message + " is: " + d.example
+        );
+      }
+    }
+	});
+};
---
+++ plugins/goodnight.js
@@ -0,0 +1,4 @@
+vexBot.commands.goodnight = function(data) {
+  data.respond("Good Night");
+  vexBot.disconnect();
+};
