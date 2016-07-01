f6443486f7645583654c27ec539478c4484020ec
Harrison Freni <hfreni@chadwickschool.org>
2016-07-01 11:03:04 -0700

    Next Update

---
+++ plugins/musicplayer.js
@@ -0,0 +1,18 @@
+
+
+vexBot.commands.playmusic = function(data) {
+	console.log("CMD Recieved");
+ 	vexBot.joinVoiceChannel(channelID, function() {
+		console.log("Chan Joined");
+    	vexBot.getAudioContext({ channel: channelID, stereo: true}, function(stream) {
+      		stream.playAudioFile(data); //To start playing an audio file, will stop when it's done.
+		});
+	});
+};
+
+/*
+stream.stopAudioFile(); //To stop an already playing file
+stream.once('fileEnd', function() {
+  //Do something now that file is done playing. This event only works for files.
+});
+*/
