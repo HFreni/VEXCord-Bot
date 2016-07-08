c840cadfed96c64cc86ff0a12572f6479e88f785
Jordan Taylor Kiesel <jtkiesel@users.noreply.github.com>
2016-07-08 01:17:57 -0400

    Update musicplayer.js

--- plugins/musicplayer.js
+++ plugins/musicplayer.js
@@ -71,7 +71,7 @@ function convertFlvToMp3(source_file, destination_dir, callback) {
 
 
 var dm = function() {
-  downloadVideo: function(url, dir_dest, file_dest, callback) {
+  downloadVideo = function(url, dir_dest, file_dest, callback) {
     var stream = ytdl(url).pipe(fs.createWriteStream(dir_dest + file_dest));
     stream.on('finish', function () {
       convertFlvToMp3(dir_dest + file_dest, dir_dest, callback);
