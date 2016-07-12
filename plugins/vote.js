31460907357dac33276c7430c0dbf842c78399c9
Harrison Freni <hfreni@hfreni.space>
2016-07-12 14:37:02 -0700

    I hate this

--- plugins/vote.js
+++ plugins/vote.js
@@ -3,9 +3,17 @@ var request = require('request');
 // In order to separate our request, we must create a way to check for a list.
 // Command Style:
 // .poll <Poll Title>; <[ArrayOfOptions]>
+// Regex for comma separation: /,(?=\w+:)/
 vexBot.commands.poll = function(data) {
-    var poll = {title: data.message, options: ['Option 1', 'Option 2']};
+    for(var a = 0; a < data.message.length; a++) {
+        if(data.message.includes(",", a) == true) {
+            console.log("True at: " + a);
+        } else {
+            console.log("False at" + a);
+        }
+    }
 
+    var poll = {title: data.message, options: ['Option 1', 'Option 2']};
     request.post({
         url: 'https://strawpoll.me/api/v2/polls',
         followAllRedirects: true,
