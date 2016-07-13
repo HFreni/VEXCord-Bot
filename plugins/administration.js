044ec1851691021e1fbd44e6662109a028c7ce7d
Harrison Freni <hfreni@hfreni.space>
2016-07-13 06:42:10 -0700

    Added boilerplate for admin tools

---
+++ plugins/administration.js
@@ -0,0 +1,18 @@
+// The goal of this module is to build in
+// the ability to moderate the chat more efficiently.
+// We will use this to keep track of current cases.
+// DQs a user has on them, and much more.
+// This will tie into the verification system, and thus
+// create a way to track all sorts of user data, and allow
+// us, the admins, to moderate the chat in a far more efficient way.
+//
+// Some examples of features:
+// * Slow Mode
+// * In-Chat DQ
+// * Auto-Mute
+// * Moderator Tools for Handling Cases.
+// * Demote/Rank Up Users.
+// * Kick/Ban Users
+// * Force Skip Music <-- See musicplayer.js
+// * Add/Remove data from the helpful links list.
+
