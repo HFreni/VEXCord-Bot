c6d2cf223e533117ef3188ab58c92feb942cc5aa
Harrison Freni <hfreni@hfreni.space>
2016-07-14 14:36:24 -0700

    Trying to work out authentication flow

--- plugins/administration.js
+++ plugins/administration.js
@@ -28,64 +28,87 @@ firebase.initializeApp({
 
 vexBot.commands.register = function(data) {
     var db = firebase.database();
-    var ref = db.ref("/userData");
+    var ref = db.ref("userData");
 	var splitChar = ";";
-
 	var lastStart = 0;
 	var numOfItems = 1;
 	var userInfo = [];
+	var userStuff = data.id;
+	var usersRef = ref.child("users");
+	var count = 0;
+	var registered = false;
+
+	// Indexing Algorithm
 	for(var a = 0; a < data.message.length; a++) {
 		if(data.message.charAt(a) == splitChar) {
-			console.log("True at: " + a);
-			console.log(data.message.substring(lastStart, a));
 			userInfo[numOfItems-1] = data.message.substring(lastStart, a);
 			numOfItems++;
 			lastStart = a+1;
 		} else {
-			console.log("False at" + a);
+			//console.log("False at" + a);
 		}
 	}
-	console.log(data.message.substring(lastStart, data.message.length));
+	//console.log(data.message.substring(lastStart, data.message.length));
 	userInfo[numOfItems-1] = data.message.substring(lastStart, data.message.length);
-	console.log(userInfo);
+	//console.log(userInfo);
 
-
-	var userStuff = data.id;
-
+	// Firebase Test Code
 	ref.on("value", function(snapshot) {
         console.log(snapshot.val());
 	}, function(errorObject) {
 		console.log("The Read failed: " + errorObject.code );
 	});
-
-    var usersRef = ref.child("users");
+	ref.on("child_added", function(snap) {
+	  count++;
+	  console.log("added:", snap.key);
+	});
+	ref.once("value", function(snap) {
+		console.log("initial data loaded!", snap.numChildren() === count);
+		console.log(count);
+	});
+	usersRef.orderByKey().on("child_added", function(snapshot) {
+		data.respond(snapshot.key);
+		if(data.id == snapshot.key) {
+			registered = true;
+		}
+		console.log(registered);
+	});
+	console.log(registered);
 
 	if(userInfo[2]) {
 		data.respond("Sorry, you have inputted too many paramaters");
 	} else {
-		if(false) {
+		console.log(registered);
+		if(registered == true) {
 			data.respond("You are already registerred");
-		} else {
+		} else if(!registered){
 			usersRef.child(userStuff).set({
+				registered: true,
 				teamNumber: userInfo[1],
 				name: userInfo[0]
 			});
 			data.respond("Congratulations, " + userInfo[0] + " you have now registered as Team#: " + userInfo[1]);
-			if(userInfo[1] == "None") {
+			if(userInfo[1].toUpperCase() == "NONE") {
 				vexBot.client.addToRole({
 					server: "197777408198180864",
 					user: data.id,
 					role: "197817210729791489"
 				});
+				vexBot.client.editNickname({
+					nick: "Testing"
+				});
+				data.respond("Added to Non Competitor Role!");
 			} else {
 				vexBot.client.addToRole({
 				    server: "197777408198180864",
 				    user: data.id,
 				    role: "197836716726288387"
 				});
+				data.respond("Added to VEX Member Role");
 				vexBot.client.editNickname({
 					nick: userInfo[0] + " | " + userInfo[1]
 				});
+				data.respond("Nick Submitted");
 			}
 		}
 	}
