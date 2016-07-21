c81a813e695b197ec7575519bc96acfa246841ff
Harrison Freni <hfreni@hfreni.space>
2016-07-21 02:42:00 -0700

    Removed redundant register

--- plugins/register.js
+++
@@ -1,8 +0,0 @@
-// This is our registration code.
-// it keeps track of user ID and team number.
-// and then promotes user to Verified if they're VEX
-// or Verified - Non Compete if not.
-// The bot will PM you on first joining the server or smth.
-//
-
-
