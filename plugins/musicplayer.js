d0dc6d73ba7c50edfe98d855e6c4330a86bf75b8
jtkiesel <rstudio@example.com>
2016-07-22 02:21:12 -0400

    Went back to old working version.

--- plugins/musicplayer.js
+++ plugins/musicplayer.js
@@ -15,10 +15,7 @@ var DOWNLOAD_DIR = './musics/';
 var audioStream = null;
 var currentVoiceChannel = null;
 var currentSong = null;
-var queue       = new Array();
-var idleQueue   = new Array("gEbcEYbxmqQ", "R5xzaDo5Q54", "UFrDpx7zLtA", "xHRkHFxD-xY", "SgM3r8xKfGE");
-var idlePos     = 0;
-var idle        = true;
+var queue       = Array();
 var skipSet     = new Set();
 
 function convertFlvToMp3(source_file, destination_dir, callback) {
@@ -70,11 +67,8 @@ function YoutubeSong(videoUrl, username, userID) {
 }
 
 function addSong(url, username, userID) {
-	idle = false;
+	joinChannel();
 
-	if (!currentVoiceChannel) {
-		joinChannel();
-	}
 	if (url && url.length > 0) {
 		var youtubeSong = new YoutubeSong(url, username, userID);
 		var exist = false;
@@ -90,7 +84,6 @@ function addSong(url, username, userID) {
 			if (exist) {
 				console.log('Music already downloaded, adding to queue... .');
 				queue.push(youtubeSong);
-
 				if (currentSong == null) {
 					start();
 				}
@@ -101,7 +94,6 @@ function addSong(url, username, userID) {
 						//sendMessage('@' + youtubeSong.username + ' Impossible to load ' + youtubeSong.url);
 					} else {
 						queue.push(youtubeSong);
-
 						if (currentSong == null) {
 							start();
 						}
@@ -114,33 +106,17 @@ function addSong(url, username, userID) {
 
 // Start the first song in the queue.
 function start() {
-	var songPath = DOWNLOAD_DIR;
-	var endFun;
-
 	if (queue.length > 0) {
 		currentSong = queue[0];
-
-		if (!currentSong || currentSong.isValid) {
-			return;
+		if (currentSong && currentSong.isValid) {
+			var songPath = DOWNLOAD_DIR + currentSong.id + '.mp3';
+			audioStream.playAudioFile(songPath);
+			skipSet.clear();
+			audioStream.once('fileEnd', songEnded);
 		}
-		songPath += currentSong.id;
-		endFun = songEnded;
 	} else {
-		songPath += idleQueue[idlePos++];
-
-		if (idlePos >= idleQueue.length) {
-			idlePos = 0;
-		}
-		endFun = start;
-	}
-	songPath += '.mp3';
-	skipSet.clear();
-
-	if (audioStream) {
-		audioStream.playAudioFile(songPath);
-		audioStream.once('fileEnd', endFun);
-	} else {
-		setTimeout(start, 500);
+		currentSong = null;
+		leaveChannel();
 	}
 }
 
@@ -170,7 +146,7 @@ function skip(userID) {
 	skipSet.add(userID);
 
 	var skipSum = skipSet.size;
-	var onlineMembers = 0;//2;
+	var onlineMembers = 2;
 /*
 	var serverID = "197777408198180864";  // Only one server.
 	for (var memberID in vexBot.servers[serverID].members) {
@@ -185,10 +161,7 @@ function skip(userID) {
 	console.log('Condition : ' + (skipSum > (onlineMembers / 2)));
 	console.log(skipSet);
 
-	if (idle) {
-		skipSet.clear();
-		stop();
-	} else if (skipSum > onlineMembers) {
+	if (skipSum > onlineMembers) {
 		if (queue.length > 0) {
 			nextSong();
 		}
@@ -219,9 +192,6 @@ function joinChannel() {
 			audioStream = stream;
 		});
 	});
-	if (idle) {
-		start();
-	}
 }
 
 function leaveChannel() {
@@ -246,11 +216,7 @@ vexBot.commands.play = function(data) {
 	addSong(data.message, data.name, data.id);
 }
 vexBot.commandUsage.play = "<youtube url>";
-vexBot.commandDescs.play = "Plays audio from a YouTube video (or adds it to the queue).";
-
-vexBot.commands.queue = function(data) {
-	return queue;
-}
+vexBot.commandDescs.play = "Plays the audio from a YouTube video.";
 
 vexBot.commands.skip = function(data) {
 	skip(data.id);
